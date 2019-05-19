5d843c1fd01a8fecd0c17093d89ef4b9ac12ef37
NoobsDeSroobs <magnus_elden@hotmail.com>
2019-05-19 18:47:43 +0200

    All settings showing and printing,

--- GUI/MainGUI.py
+++ GUI/MainGUI.py
@@ -2,15 +2,21 @@ from tkinter import *
 from tkinter import filedialog
 import tkinter as TKINTER
 
+
 def setSourceFolder():
-    file = filedialog.askopenfilename()
-    txt.insert(0, str(file))
+	file = filedialog.askopenfilename()
+	txt.insert(0, str(file))
 
 
-def print():
-   t = ""
-   targetsize.getvar("t")
-   print(t)
+def customPrint():
+	print("Target Size: " + str(targetsize.get()))
+	print("Vertex Limit: " + str(vertexlimit.get()))
+	print("Smoothing: " + str(smoothing.get()))
+	print("Decimate: " + str(decimate.get()))
+	print("ExportToFbx: " + str(exporttofbx.get()))
+	print("File Types: " + str(filetypetoimport.get()))
+	print("Source file path: " + str(txt.get()))
+
 
 window = Tk()
 
@@ -22,9 +28,9 @@ lbl = Label(window, text="Model source files folder")
 lbl.grid(column=0, row=0)
 
 txt = Entry(window, width=122)
-txt.grid(column=0, row=1, columnspan=3)
+txt.grid(column=1, row=0, sticky=TKINTER.W, columnspan=3)
 selectfolderbtn = Button(window, text="Select folder", command=setSourceFolder)
-selectfolderbtn.grid(column=4, row=1)
+selectfolderbtn.grid(column=2, sticky=TKINTER.W, row=0)
 
 lbl2 = Label(window, text="Settings:")
 lbl2.grid(column=0, sticky=TKINTER.W, row=4)
@@ -41,25 +47,25 @@ vertexlimit.grid(column=1, sticky=TKINTER.W, row=6)
 
 smoothinglabel = Label(window, text="smoothing")
 smoothinglabel.grid(column=0, sticky=TKINTER.W, row=7)
-smoothing = Checkbutton(window, width=122)
-smoothing.grid(column=1, sticky=TKINTER.W, row=7)
+smoothing = IntVar()
+Checkbutton(window, width=122, variable=smoothing).grid(column=1, sticky=TKINTER.W, row=7)
 
 decimatelabel = Label(window, text="decimate")
 decimatelabel.grid(column=0, sticky=TKINTER.W, row=8)
-decimate = Checkbutton(window, width=122)
-decimate.grid(column=1, sticky=TKINTER.W, row=8)
+decimate = IntVar()
+Checkbutton(window, width=122, variable=decimate).grid(column=1, sticky=TKINTER.W, row=8)
 
 exporttofbxlabel = Label(window, text="exporttofbx")
 exporttofbxlabel.grid(column=0, sticky=TKINTER.W, row=9)
-exporttofbx = Checkbutton(window, width=122)
-exporttofbx.grid(column=1, sticky=TKINTER.W, row=9)
+exporttofbx = IntVar()
+Checkbutton(window, width=122, variable=exporttofbx).grid(column=1, sticky=TKINTER.W, row=9)
 
 filetypetoimportlabel = Label(window, text="filetypetoimport")
 filetypetoimportlabel.grid(column=0, sticky=TKINTER.W, row=10)
 filetypetoimport = Entry(window, width=122)
 filetypetoimport.grid(column=1, sticky=TKINTER.W, row=10)
 
-startbtn = Button(window, text="Start", command=print)
+startbtn = Button(window, text="Start", command=customPrint)
 startbtn.grid(column=0, sticky=TKINTER.W, row=11)
 
 window.mainloop()
