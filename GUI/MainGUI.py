4a66f4fef7370aed691a0bd798b19c6f0e4295b2
NoobsDeSroobs <magnus_elden@hotmail.com>
2019-05-19 15:34:29 +0200

    All settings added. Not yet tested.

--- GUI/MainGUI.py
+++ GUI/MainGUI.py
@@ -5,27 +5,12 @@ def setSourceFolder():
     file = filedialog.askopenfilename()
     txt.insert(0, str(file))
 
-#Make these into hardcoded settings.
-fields = ["targetsize", "vertexlimit", "smoothing", "decimate", "folderpath", "exporttofbx", "filetypetoimport"]
 
 def fetch(entries):
    for entry in entries:
       field = entry[0]
       text  = entry[1].get()
       print('%s: "%s"' % (field, text))
-
-def makeform(root, fields):
-   entries = []
-   offset = 5
-   for field in fields:
-      lab = Label(width=15, text=field, anchor='w')
-      ent = Entry(width=100)
-      lab.grid(column=0, row=offset)
-      ent.grid(column=1, row=offset)
-      entries.append((field, ent))
-      offset = offset + 1
-
-   return entries
 
 window = Tk()
 
@@ -34,23 +19,47 @@ window.grid_columnconfigure(1, minsize=10)
 window.geometry('1260x720')
 
 lbl = Label(window, text="Model source files folder")
-
 lbl.grid(column=0, row=0)
 
 btn = Button(window, text="Select folder", command=setSourceFolder)
-
 btn.grid(column=4, row=1)
 
 txt = Entry(window, width=122)
-
 txt.grid(column=0, row=1, columnspan=3)
 
 lbl2 = Label(window, text="Settings:                    ")
-
 lbl2.grid(column=0, row=4)
 
-ents = makeform(window, fields)
-window.bind('<Return>', (lambda event, e=ents: fetch(e)))
+targetsizelabel = Label(window, text="targetsize")
+targetsizelabel.grid(column=0, row=5)
+targetsize = Entry(window, width=122)
+targetsize.grid(column=1, row=5)
+
+vertexlimitlabel = Label(window, text="vertexlimit")
+vertexlimitlabel.grid(column=0, row=6)
+vertexlimit = Entry(window, width=122)
+vertexlimit.grid(column=1, row=6)
+
+smoothinglabel = Label(window, text="smoothing")
+smoothinglabel.grid(column=0, row=7)
+smoothing = Checkbutton(window, width=122)
+smoothing.grid(column=1, row=7)
+
+decimatelabel = Label(window, text="decimate")
+decimatelabel.grid(column=0, row=8)
+decimate = Checkbutton(window, width=122)
+decimate.grid(column=1, row=8)
+
+exporttofbxlabel = Label(window, text="exporttofbx")
+exporttofbxlabel.grid(column=0, row=9)
+exporttofbx = Checkbutton(window, width=122)
+exporttofbx.grid(column=1, row=9)
+
+filetypetoimportlabel = Label(window, text="filetypetoimport")
+filetypetoimportlabel.grid(column=0, row=10)
+filetypetoimport = Entry(window, width=122)
+filetypetoimport.grid(column=1, row=10)
+
 
 
 window.mainloop()
